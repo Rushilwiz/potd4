017a3c6d34d82c7c89c17c305a6841df03e39b9e
Rushil Umaretiya <rushilwiz@gmail.com>
2026-03-17 03:27:59 -0400

    chore: fixed margin on home

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import VisitorTable from "@/components/VisitorTable";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center m-4">
-        <h1 className="text-4xl font-bold mb-4">welcome to the visitor management system</h1>
+      <div className="flex flex-col items-center justify-center mt-4">
+        <h1 className="text-4xl font-bold">welcome to the visitor management system</h1>
       </div>
       <div className="flex flex-col gap-4 px-4">
         <VisitorForm />
